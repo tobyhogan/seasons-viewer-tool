8b0418bf0c72d362bbabc81617180e799b4daf47
Tharass Imus <tobyhogan44@gmail.com>
2025-05-10 13:23:42 +0100

    not sure if this commit works, the last one - commit 123 works, the problem is tailwind has messed it up

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\Tharass\Documents\1-documents-general\coding-projects\seasons-viewer-tool\tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
